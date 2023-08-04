c2a758db96fa8dd9de8d30a73dc486b750c18a53
Iamneo <techadmin@iamneo.ai>
2023-08-04 06:16:38 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,46 +1,9 @@
-import './App.css';
-import { useState } from 'react';
 import Stopwatch from './components/Stopwatch/Stopwatch';
 
 function App() {
-  const [time, setTime] = useState({s:0, m:0, h:0});
-  const [interv,setInterv] = useState();
-  const[status,setStatus] = useState(0)
-
-  const start =() =>{
-    run();
-    setStatus(1);
-    setInterv(setInterval(run,1000));
-  }
-  const pause = ()=>{
-    clearInterval(interv);
-    setStatus(2);
-  }
-  const reset=()=>{
-    clearInterval(interv);
-    setStatus(0);
-    setTime({s:0, m:0, h:0});
-  }
-  const resume = () => start();
-  
-  var uS = time.s, uM = time.m, uH = time.h;
-
-  const run = () =>{
-    if(uM===60){
-      uH++;
-      uM=0;
-    }
-    if(uS===60){
-      uM++;
-      uS=0;
-    }
-    uS++;
-    return setTime({s:uS, m:uM, h:uH})
-  }
-
   return (
     <div className="App">
-     <Stopwatch time={time} start={start} pause={pause} reset={reset} resume={resume} status={status}/>
+      <Stopwatch />
     </div>
   );
 }
